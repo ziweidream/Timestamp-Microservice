26c75f8158fae35b9b3795480ea6327517b8a7a9
Glitch (supreme-scourge) <none>
2018-02-07 17:08:14 +0000

    🎙🌵 Checkpoint server.js:621255/295

--- server.js
+++ server.js
@@ -20,14 +20,23 @@ app.get("/:num", function (request, response) {
   
   var myobj = {};
   if (/\s/.test(request.params.num)) {
-    myobj.unix = new Date(request.params.num).getTime()/1000;
-    myobj.natural = request.params.num;
+    if (new Date(request.params.num) !== null) {
+      myobj.unix = new Date(request.params.num).getTime()/1000;
+      myobj.natural = request.params.num;
+    } else {
+      myobj.unix = null;
+      myobj.natural = null;
+    }
   } else {
+    if (new Date(parseInt(request.params.num)!==null) {
       myobj.unix = parseInt(request.params.num);   
       var str = new Date(parseInt(request.params.num)*1000);
       var locale = "en-us",
       month = str.toLocaleString(locale, { month: "long" });
       myobj.natural = str.getDate() + ' ' + month + ', ' + str.getFullYear();
+  } else {
+      myobj.unix = null;
+      myobj.natural = null;
   }
   
   response.send(myobj);
