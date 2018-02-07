6a3eb82c80e5fc0a968c17ae7e6c6c84f67bcb3c
Glitch (supreme-scourge) <none>
2018-02-07 18:00:43 +0000

    ⛪️🎨 Checkpoint server.js:621255/235

--- server.js
+++ server.js
@@ -17,27 +17,25 @@ app.get("/", function (request, response) {
 });
 
 app.get("/:num", function (request, response) {
-  
+  var valid = (new Date(timestamp)).getTime() > 0;
   var myobj = {};
   if (/\s/.test(request.params.num)) {
-    if (new Date(request.params.num) !== null) {
+      
       myobj.unix = new Date(request.params.num).getTime()/1000;
+      if (myobj.unix > 0) {
       myobj.natural = request.params.num;
-    } else {
-      myobj.unix = null;
-      myobj.natural = null;
-    }
+      } else {myobj.natural = null;}
   } else {
-    if (new Date(parseInt(request.params.num))!==null) {
-      myobj.unix = parseInt(request.params.num);   
       var str = new Date(parseInt(request.params.num)*1000);
+      if (str > 0) {
+      myobj.unix = parseInt(request.params.num);     
+    
       var locale = "en-us",
       month = str.toLocaleString(locale, { month: "long" });
-      myobj.natural = str.getDate() + ' ' + month + ', ' + str.getFullYear();
-  } else {
-      myobj.unix = null;
-      myobj.natural = null;
-  }
+      myobj.natural = str.getDate() + ' ' + month + ', ' + str.getFullYear();} else {
+        myobj.unix = null;
+        myobj.natural = null;
+  } }
   
   response.send(myobj);
 });
